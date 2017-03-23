8dd62840a965ebf9781d5cf99544f0f53e0ced5f
Hermish <hermish.mehta@mail.utoronto.ca>
2017-03-23 14:31:50 -0400

    Added methods to implement in Board.java

--- Minesweeper/src/Board.java
+++ Minesweeper/src/Board.java
@@ -1,3 +1,57 @@
 public class Board {
-	// TODO: Everything
+	private int size;
+	private Cell[][] board;
+
+	public Board(int size) {
+		// TODO
+	}
+
+	public Board(int size, double density) {
+		// TODO
+	}
+
+	public Board(String filename) throws Exception {
+		// TODO
+	}
+
+	private static Cell readCell(String input) throws Exception{
+		// TODO
+		return new Cell();
+	}
+
+	private static boolean isValidBoard(Cell[][] grid) {
+		// TODO
+		return true;
+	}
+
+	private void placeMines() {
+		// TODO
+	}
+
+	private void generateNumbers() {
+		// TODO
+	}
+
+	private boolean isMine(int row, int col) {
+		// TODO
+		return true;
+	}
+
+	@Override 
+	public String toString() {
+		// TODO
+		return "";
+	}
+
+	public void checkCell(int row, int col) {
+		// TODO
+	}
+
+	public void markCell(int row, int col) {
+		// TODO
+	}
+
+	public void save(String filename) {
+		// TODO
+	}
 }
