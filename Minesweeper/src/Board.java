7b3499abcab6f89f0141637852bde4f2d75c56c4
Hermish <hermish.mehta@mail.utoronto.ca>
2017-03-23 15:47:45 -0400

    Finished most of the IO operations/methods in the Board class, included necessary methods to be implemented in the Cell class

--- Minesweeper/src/Board.java
+++ Minesweeper/src/Board.java
@@ -1,22 +1,51 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Board {
-	private int size;
+	private int rows;
+	private int cols;
 	private Cell[][] board;
 
+	private static final double DEFAULT_DENSITY = 0.15;
+
 	public Board(int size) {
-		// TODO
+		this(size, DEFAULT_DENSITY);
 	}
 
 	public Board(int size, double density) {
 		// TODO
 	}
 
-	public Board(String filename) throws Exception {
-		// TODO
-	}
+	public Board(File inputFile) throws Exception{
+		Scanner inFile = new Scanner(inputFile);
+		
+		String line = inFile.nextLine();
+		String[] sizes = line.split(" ");
+		int width = Integer.parseInt(sizes[0]);
+		int length = Integer.parseInt(sizes[1]);
+		Cell[][] grid = new Cell[width][length];
+		int row = 0;
 
-	private static Cell readCell(String input) throws Exception{
-		// TODO
-		return new Cell();
+		for (int r = 0; r < width; r++) {
+			line = inFile.nextLine();
+			for (int c = 0; c < length; c++) {
+				char code = line.charAt(c);
+				grid[r][c] = Cell.parseCell(code);
+			}
+		}
+
+		inFile.close();
+
+		if (isValidBoard(grid)) {
+			rows = width;
+			cols = length;
+			board = grid;
+		} else {
+			throw new IOException();
+		}
 	}
 
 	private static boolean isValidBoard(Cell[][] grid) {
@@ -33,14 +62,26 @@ public class Board {
 	}
 
 	private boolean isMine(int row, int col) {
-		// TODO
-		return true;
+		return board[row][col].isMine();
 	}
 
 	@Override 
 	public String toString() {
-		// TODO
-		return "";
+		char[][] characterMatrix = new char[rows][cols];
+		String[] rowDisplays = new String[rows];
+
+		for (int row = 0; row < rows; row++) {
+			for (int col = 0; col < cols; col++) {
+				characterMatrix[row][col] = board[row][col].toChar();
+			}
+		}
+
+		for (int row = 0; row < rows; row++) {
+			rowDisplays[row] = new String(characterMatrix[row]);
+		}
+
+		String output = String.join("\n", rowDisplays);
+		return output;
 	}
 
 	public void checkCell(int row, int col) {
@@ -51,7 +92,14 @@ public class Board {
 		// TODO
 	}
 
-	public void save(String filename) {
-		// TODO
+	public void save(File outFile) throws IOException{
+		String header = String.format("%s %s\n", rows, cols);
+		String body = this.toString();
+
+		FileWriter output = new FileWriter(outFile);
+		PrintWriter writer = new PrintWriter(output);
+
+		writer.print(header);
+		writer.print(body);
 	}
 }
--- Minesweeper/src/Cell.java
+++ Minesweeper/src/Cell.java
@@ -1,3 +1,18 @@
 public class Cell {
 	// TODO: Everything
+
+	public static Cell parseCell(char code) throws Exception{
+		// TODO
+		return new Cell();
+	}
+
+	public char toChar() {
+		// TODO
+		return ' ';
+	}
+
+	public boolean isMine() {
+		// TODO
+		return true;
+	}
 }
